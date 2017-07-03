94acf1875735cce48124b5a2a042316b9f4dbacd
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-07-03 14:15:04 +0300

    test function returns error message if arg not integer

---
+++ Generate_prime_numbers_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from Generate_prime_numbers import generatePrimeNumbers
+
+
+class TestGeneratePrimeNumbers(unittest.TestCase):
+    def setUp(self):
+        self.arg_limit = 32
+        self.sample_prime = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31]
+        self.sample_prime_length = 11
+
+    def test_function_returns_error_message_if_arg_not_integer(self):
+        self.assertRaises(TypeError, generatePrimeNumbers, 'two')
